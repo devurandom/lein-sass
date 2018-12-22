a55a1a55e738d0d8a91a4b0923a5b4a80ad2bd8a
Dennis Schridde <devurandom@gmx.net>
2018-12-22 11:17:45 +0100

    Add dependency on Kaocha and setup CircleCI and Codecov

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/yogthos/lein-sass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "0.0-319"]
+                                     [lambdaisland/kaocha-cloverage "0.0-22"]
+                                     [lambdaisland/kaocha-junit-xml "0.0-47"]]}}
+  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
