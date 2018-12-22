d65822ccd596517657176680c590fc51949f9488
Dennis Schridde <devurandom@gmx.net>
2018-12-22 13:02:12 +0100

    Test whether we create files and whether their content is as expected
    FIXME: Calling `(sass ...)` will only work the first time it is called.
    Afterwards it will do nothing and return `nil`.

--- test/integration/lein_sass/core_test.clj
+++ test/integration/lein_sass/core_test.clj
@@ -1,7 +1,31 @@
 (ns lein-sass.core-test
-  (:require [clojure.test :refer :all]
-            [leiningen.sass :refer :all]))
+  (:require
+    [clojure.java.io :as io]
+    [clojure.test :refer :all]
+    [leiningen.sass :refer :all])
+  (:import
+    (java.io File)))
 
+(def target-css-file (io/file "target/test.css"))
+(def target-css-map-file (io/file "target/test.css.map"))
+
+; FIXME: Calling `(sass ...)` will only work the first time it is called.
+;        Afterwards it will do nothing and return `nil`.
 (deftest test-compiler
-  (is (sass {:sass {:source "test/test.scss"
-                    :target "target/"}})))
+  (testing "Basics"
+    (.delete target-css-file)
+    (.delete target-css-map-file)
+    (is (true? (sass {:sass {:source "test/test.scss"
+                             :target "target/"}}))
+        "does not crash")
+    (is (.exists target-css-file)
+        "creates a CSS file")
+    (is (.exists target-css-map-file)
+        "creates a CSS Source Map file"))
+  (testing "Content"
+    (is (= ".selector {\n  margin: 10px; }\n  .selector .nested {\n    margin: 5px; }\n\n/*# sourceMappingURL=test.css.map */\n"
+           (slurp target-css-file))
+        "outputs CSS")
+    (is (= "{\n  \"version\": 3,\n  \"file\": \"test.css\",\n  \"sources\": [\n    \"../test/test.scss\"\n  ],\n  \"mappings\": \"AAEA,AAAA,SAAS,CAAC;EACR,MAAM,EAHF,IAAI,GAOT;EALD,AAEE,SAFO,CAEP,OAAO,CAAC;IACN,MAAM,EAAE,GAAI,GACb\",\n  \"names\": []\n}"
+           (slurp target-css-map-file))
+        "outputs CSS Source Map")))
